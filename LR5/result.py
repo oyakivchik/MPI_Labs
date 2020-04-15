1c39d2419e63faeda5713bb5c9428e59f9079768
Oleksii Yakivchik <oleksii.yakivchik@yahoo.com>
2020-04-15 17:12:16 +0300

    LR5

---
+++ LR5/result.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import subprocess
+import os
+import time
+import socket
+import sys
+import multiprocessing
+
+num_cpus = multiprocessing.cpu_count()
+num_of_processes = [2, 10, 100, int(num_cpus/2), num_cpus]
+num_of_processes.sort()
+intervals=[]
+for entry in num_of_processes:
+    TASK_OUTPUT = subprocess.run(["mpiexec", "-n", "8", "python3", "task.py"], stdout=subprocess.PIPE, universal_newlines=True).stdout
+    TASK_ENTRIES = TASK_OUTPUT.split('\n')
+    intervals.append(float(TASK_ENTRIES[1]))
+print(intervals)
+plt.figure(figsize=(20,10))
+plt.plot(num_of_processes, intervals)
+plt.xticks(num_of_processes)
+#plt.yticks(intervals)
+plt.ylabel('Intervals')
+plt.xlabel('Number of processes')
+plt.savefig('plot.png')
+plt.show()
---
+++ LR5/task.py
@@ -0,0 +1,55 @@
+from mpi4py import MPI
+import random
+import time
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+size1 = 1000
+size2 = 1000
+
+
+if rank == 0:
+    matrix = []
+    random.seed(1)
+    for i in range(size1):
+        matrix.append([])
+    for i in range(size1):
+        for j in range(size2):
+            matrix[i].append(random.randrange(1, 101))
+    parts = []
+    for i in range(size):
+        parts.append(int(size1/size))
+    for i in range(size1 % size):
+        parts[i] = parts[i]+1
+    ranges = []
+    index = 0
+    for i in range(len(parts)):
+        if i == 0:
+            ranges.append(range(parts[i]))
+            index = parts[i]
+        else:
+            ranges.append(range(index, index+parts[i]))
+            index = index + parts[i]
+    data = {'matrix': matrix, 'ranges': ranges}
+else:
+    data = None
+
+data = comm.bcast(data, root=0)
+summary = 0
+start = time.time()
+for i in data['ranges'][rank]:
+    if i%2==0:
+        summary = summary + sum(data['matrix'][i])
+finish = time.time()
+interval = finish - start
+
+interval = comm.reduce(interval, MPI.MAX, root=0)
+summary = comm.reduce(summary, MPI.SUM, root=0)
+
+if rank == 0:
+    print (f"{summary}\n{interval}")
+else:
+    assert summary is None
+    assert interval is None
